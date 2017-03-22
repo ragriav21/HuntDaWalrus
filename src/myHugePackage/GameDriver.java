15a1ec8001e239399ec9ec21fc24efe60349a580
Rafael <ragriav@gmail.com>
2017-03-22 11:33:14 -0400

    Wrote GameDriver instructions and created Hero Class

--- src/myHugePackage/GameDriver.java
+++ src/myHugePackage/GameDriver.java
@@ -8,20 +8,24 @@ public class GameDriver {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter the length/width of the map grid you want: ");
 		int dimensions = scanner.nextInt();
-		GameMap gameMap = generateGameMap(dimensions);
+		GameMap gameMap = new GameMap(dimensions);
+		gameMap.addRandomBlockedSpaces();
 		boolean isGameContinuing = true;
+		printDirections();
 		while(isGameContinuing) {
-			
+			System.out.println("In which direction do you want to move?");
 		}
 	}
 	
-	public static GameMap generateGameMap(int dimensions) {
-		GameMap gameMap = new GameMap(dimensions);
-		int numberOfBlockedSpaces = dimensions < 4 ? 1 : dimensions;
-		
+	public static void printDirections() {
+		System.out.println("Welcome to Hunt the Walrus! The objective of the game is to either: ");
+		System.out.println("a) Kill the Walrus using the bows and arrows with which you're equipped, ");
+		System.out.println("b) Escape the maze by reaching the hidden exit (you'll see a light when you're near it).");
+		System.out.println("Each turn, you have the option of either trying to reach the exit by typing one of four directional keys into the command line");
+		System.out.println("('w' to go up, 'a' to go left, 's' to go down, and 'd' to go right), or to try and kill the Walrus ");
+		System.out.println("(he's hunting you, and you can't see him but you can see his victims, hear him, and at worse, smell him), by shooting your arrows in the direction you think he's located");
+		System.out.println("(type 'q' to access your weapons, and then type 'w' to shoot up, 'a' to shoot left, 's' to shoot down, and 'd' to shoot right, with bows reaching up to two spaces in one direction).");
+		System.out.println("Watch out for bats, who'll fly you to random locations, and pits, from which you cannot escape if you fall in!");
 	}
-	
-	public static Coordinate[] addBlockedSpaces(int numberOfBlockedSpaces) {
-		Coordinate[] blockedSpaces = new Coordinate[numberOfBlockedSpaces]
-	}
+
 }
--- src/myHugePackage/GameMap.java
+++ src/myHugePackage/GameMap.java
@@ -11,16 +11,33 @@ public class GameMap {
 		blockedSpaces = new Stack<Coordinate>();
 	}
 
-	public boolean isPlayerMoveValid(Coordinate c) {
-		if (c.getX() >= dimension || c.getY() >= dimension || c.getX() < 0 || c.getY() < 0)
+	public boolean isPlayerMoveValid(Coordinate coordinate) {
+		if (!isCoordinateWithinBounds(coordinate))
 			return false;
 		for (Coordinate block : blockedSpaces) {
-			if (c.getX() == block.getX() && c.getY() == block.getY()) 
+			if (coordinate.getX() == block.getX() && coordinate.getY() == block.getY()) 
 				return false;
 		}
 		return true;
 	}
-	public void addBlockedSpace(Coordinate c) {
-		blockedSpaces.push(c);
+	
+	public void addRandomBlockedSpaces() {
+		int numberOfBlockedSpaces = dimension < 4 ? 1 : dimension;
+		for(int i = 0; i < numberOfBlockedSpaces; i++) {
+			int x = (int) (Math.random() * dimension);
+			int y = (int) (Math.random() * dimension);
+			Coordinate randomCoordinate = new Coordinate(x, y);
+			addBlockedSpace(randomCoordinate);
+		}
+	}
+	
+	public void addBlockedSpace(Coordinate coordinate) {
+		if(isCoordinateWithinBounds(coordinate))
+			blockedSpaces.push(coordinate);
+	}
+	
+	private boolean isCoordinateWithinBounds(Coordinate coordinate) {
+		return (coordinate.getX() >= dimension || coordinate.getY() >= dimension || 
+				coordinate.getX() < 0 || coordinate.getY() < 0);
 	}
 }    
