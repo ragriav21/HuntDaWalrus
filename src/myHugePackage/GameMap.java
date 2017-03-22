f7cc4d1c83bd5ae28e2238ebbf9b8491aab7c6d8
Kenny Wong <0kennywong@gmail.com>
2017-03-22 10:12:57 -0400

    changed gamemap

--- src/myHugePackage/GameMap.java
+++ src/myHugePackage/GameMap.java
@@ -2,21 +2,23 @@ package myHugePackage;
 
 public class GameMap {
 	private int dimension;
-	private Coordinate[] spacesWithBlocks;
+	private Stack<Coordinate> blockedSpaces;
 	
-	public GameMap() {
-		dimension = 3;
-		spacesWithBlocks = new Coordinate[1];
-		spacesWithBlocks[0] = new Coordinate(1, 1);
+	public GameMap(int dimension) {
+		dimension = dimension;
+		blockedSpaces = new Stack<Coordinate>();
 	}
 
 	public boolean isPlayerMoveValid(Coordinate c) {
 		if (c.getX() >= dimension || c.getY() >= dimension)
 			return false;
-		for (int i = 0; i < spacesWithBlocks.length; i++) {
-			if (spacesWithBlocks[i].getX() == c.getX() && spacesWithBlocks[i].getY() == c.getY())
+		for (Coordinate block : blockedSpaces) {
+			if (c.getX() == block.getX() && c.getY() == block.getY()) 
 				return false;
 		}
 		return true;
 	}
-}
+	public void addBlockedSpace(Coordinate c) {
+		blockedSpaces.push(c);
+	}
+}    
