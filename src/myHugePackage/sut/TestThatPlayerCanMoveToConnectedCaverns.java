907bce772610ae96f1b02a9661cce9db4ee3b4a2
Kenny Wong <0kennywong@gmail.com>
2017-03-23 09:23:08 -0400

    first fixture

--- src/myHugePackage/sut/TestThatPlayerCanMoveToConnectedCaverns.java
+++ src/myHugePackage/sut/TestThatPlayerCanMoveToConnectedCaverns.java
@@ -1,8 +1,7 @@
 public class TestThatPlayerCanMoveToConnectedCaverns {
 	private int x0;
 	private int y0;
-	private int x1;
-	private int y1;
+	private Hero hero;
 	private String direction; 
 
 	public void setX0(int x) {
@@ -11,19 +10,18 @@ public class TestThatPlayerCanMoveToConnectedCaverns {
 	public void setY0(int y) {
 		y0 = y;
 	}
-	public void setX1(int x) {
-		x1 = x;
+	public int X1() {
+		return hero.getCurrentPosition().getX();
 	}
-	public void setY1(int y) {
-		y1 = y;
+	public int Y1() {
+		return hero.getCurrentPosition().getY();
 	}
 	public void setDirection(String dir) {
 		direction = dir;
 	}
 	public void execute() {
 		Coordinate startingPosition = new Coordinate(x0, y0);
-		Hero hero = new Hero(startingPosition);
+		hero = new Hero(startingPosition);
 		hero.updatePosition(direction);
-		return hero.getCurrentPosition();
 	}
 }
